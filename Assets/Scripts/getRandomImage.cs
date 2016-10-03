3130829b9f4c8670a8b7fdbd2d25df267920c670
RinkeNuytenLaptop <rinke.nuyten@hotmail.com>
2016-10-03 14:47:57 +0200

    V1

---
+++ Assets/Scripts/getRandomImage.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class getRandomImage : MonoBehaviour {
+
+    public Image Stone;
+    public Image Scissors;
+    public Image Paper;
+
+	// Use this for initialization
+	void Start () {
+        Stone.enabled = false;
+        Scissors.enabled = false;
+        Paper.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void enableRandomPic()
+    {
+        int rand = UnityEngine.Random.Range(0, 3);
+        if(rand == 0)
+        {
+            Stone.enabled = true;
+            Scissors.enabled = false;
+            Paper.enabled = false;
+        }
+        else if(rand == 1)
+        {
+            Stone.enabled = false;
+            Scissors.enabled = true;
+            Paper.enabled = false;
+        }
+        else
+        {
+            Stone.enabled = false;
+            Scissors.enabled = false;
+            Paper.enabled = true;
+        }
+    }
+}
